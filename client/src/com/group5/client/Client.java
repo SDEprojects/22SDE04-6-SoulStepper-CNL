50aefeac7a00a5dd8eed154244cc66bdc5c368e1
Lesly <brissonlesly@gmail.com>
2022-08-02 20:04:18 -0400

    initial commit

---
+++ client/src/com/group5/client/Client.java
@@ -0,0 +1,11 @@
+package com.group5.client;
+
+public class Client {
+    public static void main(String[] args) throws InterruptedException {
+        //initiating game class
+        Game game = new Game();
+        //calling play method from game class.
+        game.play();
+
+    }
+}
